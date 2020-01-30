3bd55272a6e42806c6e52ec311f748770736063e
Miguel Alcântara <miguel.alcantara@innowave.tech>
2020-01-30 11:24:58 +0000

    Added snippets

---
+++ Snippets/importSnippets.sh
@@ -0,0 +1,21 @@
+# Xcode Snippets directory
+XCODE_SNIPPETS_DIR="${HOME}/Library/Developer/Xcode/UserData/CodeSnippets"
+
+# Check if there are code snippets to copy in the current directory.
+if [[ -z `ls *.codesnippet` ]] 
+then
+	echo "No code snippets in current directory. Exiting."
+	exit 1;
+fi
+
+# Create code snippets destination directory, if it does not exist.
+if ! [[ -d $XCODE_SNIPPETS_DIR ]]
+then
+    echo "$XCODE_SNIPPETS_DIR does not exist. Creating the directory."
+    mkdir $XCODE_SNIPPETS_DIR
+else 
+	echo "$XCODE_SNIPPETS_DIR exists."
+fi
+
+# Copy all the code snippets to the Xcode snippets directory
+cp *.codesnippet $XCODE_SNIPPETS_DIR
