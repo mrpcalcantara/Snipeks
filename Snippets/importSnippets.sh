0f33e115ec089868dc4e41c0d1242cb9126b5775
Miguel Alcântara <miguel.alcantara@innowave.tech>
2020-02-05 11:00:26 +0000

    Added export snippets script and included new weak/strong dance snippet

--- Snippets/importSnippets.sh
+++ Snippets/importSnippets.sh
@@ -1,12 +1,18 @@
+CURRENT_DIR=`pwd`
+
 # Xcode Snippets directory
 XCODE_SNIPPETS_DIR="${HOME}/Library/Developer/Xcode/UserData/CodeSnippets"
 
 # Check if there are code snippets to copy in the current directory.
+cd $XCODE_SNIPPETS_DIR
 if [[ -z `ls *.codesnippet` ]] 
 then
 	echo "No code snippets in current directory. Exiting."
 	exit 1;
 fi
+
+cd $CURRENT_DIR
+echo $CURRENT_DIR
 
 # Create code snippets destination directory, if it does not exist.
 if ! [[ -d $XCODE_SNIPPETS_DIR ]]
